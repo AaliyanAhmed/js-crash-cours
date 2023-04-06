7e8b4cb06bcc6c2f9f069da26953403f0091018c
Aaliyan Ahmed <121717147+AaliyanAhmed@users.noreply.github.com>
2023-04-06 18:39:55 +0500

    assignment 1

---
+++ app.js
@@ -0,0 +1,142 @@
+//*********************task1 ********************
+
+// let teble = +prompt('enter a table number')
+// for(let i = 1; i <= 10; i++){
+//     console.log(teble + " X " + i + " = " + teble*i)
+// }
+
+//**********************task2**********************
+
+// let yourAge = prompt('enter your age');
+// if (yourAge <= 10 ) {
+//   alert('your Allowed')
+// }
+// else{
+//   alert('your not Allowed')
+// }
+
+
+//*************************Question no 1************************
+
+// let num1 = Number (prompt("Enter first number:"));
+// let num2 = Number (prompt("Enter second number:"));
+
+// if (num1 > num2 ) {
+//   console.log("The larger number is: " + num1);
+// } else if (num2 > num1) {
+//   console.log("The larger number is: " + num2);
+// }else{
+//   console.log("pleas enter a number")
+// }
+
+
+//************************Question no 2*********************
+
+// let num = Number (prompt("Enter a number:"));
+
+// if (num > 0) {
+//   alert("The numbre is plus " + num);
+// } else if (num < 0) {
+//   alert("The number  is minus " + num);
+// } else {
+//   alert("The number is zero");
+// }
+
+
+//***************Question no 3******************
+
+// let num1 = prompt("Enter first number:");
+// let num2 = prompt("Enter second number:");
+// let num3 = prompt("Enter third number:");
+// let num4 = prompt("Enter fourth number:");
+// let num5 = prompt("Enter fifth number:");
+
+// let max = num1;
+
+// if(max > num2){
+//   max = num2
+// }
+// if (num2 > max) {
+//   max = num2;
+// }
+
+// if (num3 > max) {
+//   max = num3;
+// }
+
+// if (num4 > max) {
+//   max = num4;
+// }
+
+// if (num5 > max) {
+//   max = num5;
+// }
+
+// console.log("The largest number is: " + max);
+
+
+//********************Question no 4*********************
+
+// for (let i = 0; i <= 15; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i + " is even");
+//   } else {
+//     console.log(i + " is odd");
+//   }
+// }
+
+// ***********************Question no 5********************
+
+
+// let marks = [80, 70, 90, 85, 95];
+
+// let total = 0;
+// for (let i = 0; i < marks.length; i++) {
+//   total += marks[i];
+// }
+// let average = total / marks.length;
+
+// let grade;
+// if (average >= 100) {
+//   grade = "A";
+// } else if (average >= 90) {
+//   grade = "B";
+// } else if (average >= 80) {
+//   grade = "C";
+// } else if (average >= 70) {
+//   grade = "D";
+// } else {
+//   grade = "F";
+// }
+
+// console.log("The average mark is: " + average);
+// console.log("The corresponding grade is: " + grade);
+
+
+
+// ******************Question no 6*************************
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 == 0 && i % 5 == 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 == 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 == 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// **************************Question no 7***********************
+// let rows = 5;
+
+// for (let i = 1; i <= rows; i++) {
+//   let pattern = "";
+//   for (let j = 1; j <= i; j++) {
+//     pattern += "* ";
+//   }
+//   console.log(pattern);
+// }
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Done~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
